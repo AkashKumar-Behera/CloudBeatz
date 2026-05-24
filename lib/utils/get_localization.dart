8d4c3d0f9b39d8cd4be4603e7b07c9da2cd1860d
Akash Kumar Behera <akashkumar48874@gmail.com>
2026-05-24 16:28:11 +0530

    refactor: rename remaining occurrences of Harmony Music to CloudBeatz

--- lib/utils/get_localization.dart
+++ lib/utils/get_localization.dart
@@ -6607,7 +6607,7 @@ Map<String, Map<String, String>> get keys => {
     "videos": "Teledyski",
     "viewAll": "Pokaż wszystko",
     "results": "Wyniki",
-    "about": "O Harmony Music",
+    "about": "O CloudBeatz",
     "songRemovedfromQueue": "Usunięto z kolejki!",
     "radioNotAvailable": "Radio niedostępne dla tego wykonawcy!",
     "songRemovedfromQueueCurrSong": "Nie można usunąć aktualnie odtwarzanego utworu",
